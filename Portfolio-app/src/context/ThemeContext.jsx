3db6089126de8e9682316bbc690fd180d4f44c39
Adrien Le Guen <adri1.leguen@gmail.com>
2026-01-02 22:40:53 +0100

    Test pour vercel.

--- Portfolio-app/src/context/ThemeContext.jsx
+++ Portfolio-app/src/context/ThemeContext.jsx
@@ -1,37 +1,33 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect } from "react";
 
 export const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
-    const [darkMode, setDarkMode] = useState(false);
+  // Initialiser directement avec la valeur du localStorage
+  const [darkMode, setDarkMode] = useState(() => {
+    const storedTheme = localStorage.getItem("darkMode");
+    return storedTheme ? JSON.parse(storedTheme) : false;
+  });
 
-    useEffect(() => {
-        // Vérifier si le thème est stocké dans localStorage
-        const storedTheme = localStorage.getItem('darkMode');
-        if (storedTheme) {
-            setDarkMode(JSON.parse(storedTheme));
-        }
-    }, []);
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    // Stocker le thème dans localStorage pour la persistance
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
 
-    useEffect(() => {
-        if (darkMode) {
-            document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-        }
-        // Stocker le thème dans localStorage pour la persistance
-        localStorage.setItem('darkMode', JSON.stringify(darkMode));
-    }, [darkMode]);
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
 
-    const toggleDarkMode = () => {
-        setDarkMode(prevMode => !prevMode);
-    };
-
-    return (
-        <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
-            {children}
-        </ThemeContext.Provider>
-    );
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
 export default ThemeProvider;
